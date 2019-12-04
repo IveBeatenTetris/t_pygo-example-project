c60d8e3a6642c72dae47b16aabf6fad07a5fa896
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2019-12-04 13:56:39 +0100

    testing interface elements

--- interface_tests.py
+++ interface_tests.py
@@ -3,12 +3,19 @@ import pygame as pg
 
 # assignments
 app = go.Window({
-	"title": "window_tests",
+	"title": "interface_tests",
 	"resizable": True,
 	"size": go.getMachineResolution(),
-	"backgroundrepeat": "xy"
+	"background": (25, 25, 35)
 })
 gui = go.Interface("app_test1")
+mousepos = go.Text({
+	"font": "Verdana",
+	"fontsize": 14,
+	"text": "mouse position",
+	"antialias": True,
+	"color": (150, 150, 150)
+})
 # main loop
 def main():
 	while True:
@@ -19,8 +26,18 @@ def main():
 		if app.resize():
 			gui.resize(app.size)
 		# drawing
+		for elem in gui.elements:
+			if elem.name == "info_bar":
+				go.draw(mousepos, gui, (
+					elem.rect.left + 5,
+					elem.rect.top + 3
+				))
 		app.draw(gui)
 		# updating
+		mousepos.update({
+			"text": "Cursor: x" + str(go.getMouse()[0]) + " y" + str(go.getMouse()[1])
+		})
+		gui.update()
 		app.update()
 
 if __name__ == '__main__':
