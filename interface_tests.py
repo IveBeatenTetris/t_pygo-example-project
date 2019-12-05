eabfb9bd6784f6b379cf032fb78fb130b09b9b91
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2019-12-05 01:42:54 +0100

    testing dropdownmenu

--- interface_tests.py
+++ interface_tests.py
@@ -10,15 +10,19 @@ app = go.Window({
 	"backgroundrepeat": "xy"
 })
 gui = go.Interface("app_test1")
+menu_file = go.DropDownMenu({
+	"rect": pg.Rect(400, 400, 150, 200),
+	"background": (55, 55, 65)
+})
 mousepos = go.Text({
-	"font": "Ebrima",
+	"font": "ebrima",
 	"fontsize": 14,
 	"text": "mouse position",
 	"antialias": True,
 	"color": (150, 150, 150)
 })
 appsize = go.Text({
-	"font": "Ebrima",
+	"font": "ebrima",
 	"fontsize": 14,
 	"text": "window size",
 	"antialias": True,
@@ -28,11 +32,16 @@ appsize = go.Text({
 def main():
 	while True:
 		# events
-		app.events()
+		events = app.events()
 		if app.keys()["esc"]:
 			app.quit()
 		if app.resize():
 			gui.resize(app.size)
+		for elem in gui.elements:
+			if elem.name == "menu_main":
+				for e in elem.elements:
+					if e.leftClick(events):
+						print("click")
 		# drawing
 		for elem in gui.elements:
 			if elem.name == "info_bar":
@@ -44,6 +53,7 @@ def main():
 					160,
 					elem.rect.top + 2
 				))
+		go.draw(menu_file, gui, menu_file.rect)
 		app.draw(gui)
 		# updating
 		mousepos.update({
