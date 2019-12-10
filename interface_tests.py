f7d270c05198702709010e035134c9cfd0d0fd7b
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2019-12-10 21:38:16 +0100

    testing new interface file

--- interface_tests.py
+++ interface_tests.py
@@ -10,7 +10,7 @@ app = go.App({
 	"backgroundrepeat": "xy",
 	"fps": 100
 })
-gui = go.Interface("app_test1")
+gui = go.Interface2("app_test2")
 
 # main loop
 def main():
@@ -21,15 +21,19 @@ def main():
 			app.quit()
 		if app.resize():
 			gui.resize(app.size)
+		#menu1 = gui.menus["menu_main_file"]
+		#print(menu1.active)
+		#print(menu1.config)
+		print(gui.leftClick())
 		# drawing
 		app.draw(gui)
 		# updating
-		gui.elements["info_bar"].info = {
+		"""gui.elements["info_bar"].info = {
 			"Mouse": go.getMouse(),
 			"AppSize": app.size,
 			"FPS": app.fps
-		}
-		gui.update(events)
+		}"""
+		#gui.update(events)
 		app.update()
 
 if __name__ == '__main__':
