e68622a91467f015d364bc37b97774cd9091825c
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-05 15:56:41 +0100

    fixed background_hover not applying corretly for guimaster

--- interface_tests_2.py
+++ interface_tests_2.py
@@ -1,28 +1,29 @@
 import t_pygo as go
 import pygame as pg
 # assignments
-app = go.App({
-	"title": "interface_tests",
-	"resizable": True,
-	"size": go.getMachineResolution(),
-	#"background": (25, 25, 35),
-	"backgroundrepeat": "xy",
-	"fps": 120
-})
-gui = go.Interface("app_test4")
+app = go.App(
+	size = (580, 650),
+	#background = (35, 35, 45),
+	resizable = True
+)
+#gui = go.Interface("app_test4")
+gui = go.GuiMaster(
+	position = app.rect.center,
+	background_hover = (55, 55, 65)
+)
 # main loop
 def main():
 	while True:
 		# events
-		events = app.events()
-		if app.keys()["esc"]:
-			app.quit()
+		#if app.keys()["esc"]:
+			#app.quit()
 		if app.resize():
-			gui.resize(app.size)
+			#gui.resize(app.size)
+			gui.rect.center = app.rect.center
 		# drawing
 		app.draw(gui, gui.rect)
 		# updating
-		#gui.update()
+		gui.update()
 		app.update()
 
 if __name__ == '__main__':
