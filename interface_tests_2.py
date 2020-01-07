e7cf21ea780214f0abee5223b6f558ed16541f5b
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-07 23:40:05 +0100

    changed looping order for corretly updating app's visual content

--- interface_tests_2.py
+++ interface_tests_2.py
@@ -8,37 +8,51 @@ app = go.App(
 	fps			=	120
 )
 #gui = go.Interface("app_test4")
+layout = go.Layout(
+	size		=	app.rect.size,
+	rows		=	[
+						{
+							"size"	: (100, 50),
+							"cols"	:	[
+								{
+									"content": None
+								}
+							]
+						}
+					]
+)
 elem = go.GuiMaster(
 	parent		=	app,
 	#position = app.rect.center,
+	background = (35, 35, 45),
 	#background_hover = (55, 55, 65),
 	dragable	=	True,
 	drag_area	=	[5, 5, 290, 25],
 	resizable	=	"buttomleft"
 )
-layout = go.Layout(
-	size		=	app.rect.size,
-	background	=	(200, 200, 215)
-)
 # main loop
 def main():
+	redraw_layout = True
 	while True:
 		# -------------------------------------------------------------------- #
 		print(app.fps)
 		# -------------------------------------------------------------------- #
-		# events
-		if "esc" in app.keys:
-			app.quit()
-		if app.resize():
-			layout.resize(app.rect.size)
-		# updating elements
-		elem.update()
-		# drawing
-		#app.draw(elem, elem.rect)
-		app.draw(layout, layout.rect)
 		# updating
 		layout.update()
 		app.update()
+		# events
+		if "esc" in app.keys:
+			app.quit()
+		if app.resized:
+			layout.resize(app.rect.size)
+			redraw_layout = True
+		# updating elements
+		#elem.update()
+		# drawing
+		if redraw_layout:
+			app.draw(layout, layout.rect)
+			redraw_layout = False
+		#app.draw(elem, elem.rect)
 
 if __name__ == '__main__':
     main()
