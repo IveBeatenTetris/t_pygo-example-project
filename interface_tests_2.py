97f25ffd80d8c691f403e4bd7285c044f2d0d10d
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-08 14:48:23 +0100

    testing 'table' element

--- interface_tests_2.py
+++ interface_tests_2.py
@@ -1,25 +1,15 @@
 import t_pygo as go
 import pygame as pg
 # assignments
-app = go.App(
-	size		=	(580, 650),
-	#background = (35, 35, 45),
-	resizable	=	True,
-	fps			=	120
-)
-#gui = go.Interface("app_test4")
-layout = go.Layout(
-	size		=	app.rect.size,
-	rows		=	[
-						{
-							"size"	: (100, 50),
-							"cols"	:	[
-								{
-									"content": None
-								}
-							]
-						}
-					]
+app		=	go.App(
+				size		=	(580, 650),
+				background	= 	(35, 35, 45),
+				resizable	=	True,
+				fps			=	120
+			)
+table	=	go.Table(
+				rows		=	3,
+				cols		=	3
 )
 elem = go.GuiMaster(
 	parent		=	app,
@@ -30,29 +20,51 @@ elem = go.GuiMaster(
 	drag_area	=	[5, 5, 290, 25],
 	resizable	=	"buttomleft"
 )
+"""layout = go.Layout2(
+	size		=	app.rect.size,
+	rows		=	[
+						{
+							#"size"	: (100, 50),
+							"border":	(1, app.background),
+							"cols"	:	[
+								{
+									"border":	(1, app.background),
+									"content"	:	None
+								}
+							]
+						},
+						{
+							"border":	(1, app.background),
+							"cols"	:	[
+								{"content"	:	None}
+							]
+						},
+						{
+							"border":	(1, app.background),
+							"cols"	:	[
+								{"content"	:	None}
+							]
+						}
+					]
+				)"""
 # main loop
 def main():
-	redraw_layout = True
 	while True:
 		# -------------------------------------------------------------------- #
-		print(app.fps)
+		#print(app.fps)
 		# -------------------------------------------------------------------- #
-		# updating
-		layout.update()
-		app.update()
 		# events
 		if "esc" in app.keys:
 			app.quit()
 		if app.resized:
-			layout.resize(app.rect.size)
-			redraw_layout = True
-		# updating elements
-		#elem.update()
+			table.resize(app.rect.size)
 		# drawing
-		if redraw_layout:
-			app.draw(layout, layout.rect)
-			redraw_layout = False
+		app.draw(table, table.rect)
 		#app.draw(elem, elem.rect)
+		# updating
+		app.update()
+		table.update()
+		#elem.update()
 
 if __name__ == '__main__':
     main()
