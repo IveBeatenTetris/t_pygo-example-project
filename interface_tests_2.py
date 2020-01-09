c1c625a08fef1443d9e1fbd15717e57880c9ea94
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-09 14:44:04 +0100

    show-off for all existing elements

--- interface_tests_2.py
+++ interface_tests_2.py
@@ -2,16 +2,42 @@ import t_pygo as go
 import pygame as pg
 # assignments
 app	= go.App(
-	size		=	(580, 650),
-	background	= 	(35, 35, 45),
-	resizable	=	True,
-	fps			=	120
+	title =	"Test Project 0.1",
+	#size =	(580, 650),
+	size = go.getMachineResolution(),
+	background = (35, 35, 45),
+	resizable =	True,
+	fps = 120
 )
-text = go.Text(
-	text		=	"MyText",
-	border		=	True,
-	background	=	(40, 45, 35),
-	padding		=	10
+
+margin = 10
+elements = {}
+
+elements["table"] = go.Table(
+	rows = 3, cols = 2,
+	size = (150, 50), position = (margin, margin),
+	border = True, border_color = (180, 160, 170),
+)
+elements["button"] = go.Button(
+	text =	"New Button",
+	position = (
+		elements["table"].rect.right + margin,
+		elements["table"].rect.top
+	),
+	border = True,
+	#background = (40, 45, 35),
+	background_hover = (50, 55, 45),
+	padding = 10,
+	font_size =	20,
+)
+elements["text"] = go.Text(
+	text = "Lorem Ipsum dolor set amet.",
+	position = (
+		elements["button"].rect.right + margin,
+		elements["button"].rect.top
+	),
+	font_size =	20,
+	wrap = 200
 )
 # main loop
 def main():
@@ -23,8 +49,11 @@ def main():
 		if "esc" in app.keys:
 			app.quit()
 		# drawing
-		app.draw(text, app.rect.center)
+		for _, elem in elements.items():
+			app.draw(elem, elem.rect)
 		# updating
+		for _, elem in elements.items():
+			elem.update()
 		app.update()
 
 if __name__ == '__main__':
