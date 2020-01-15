f318d215b14cc2a55e9507690b135972c2d95743
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-15 23:52:29 +0100

    testing table

--- interface_tests_tabledraw.py
+++ interface_tests_tabledraw.py
@@ -3,53 +3,50 @@ import pygame as pg
 # assignments
 app	= go.App(
 	title =	"Testing Table-drawing",
-	size = go.getMachineResolution(),
+	#size = go.getMachineResolution(),
+	#size = (600, 400),
+	size = (600, 400),
 	background = (35, 35, 45),
 	resizable =	True,
 	fps = 120
 )
-
 elements = {}
-elements["info_panel"] = go.Table(
-	rows = 2,
-	cols = 4,
-	size = (app.rect.width, 50),
-	border = True,
-	background = (50, 50, 60)
+elements["text_test"] = go.Text(text="my new text")
+elements["panel_drag"] = go.Panel(
+	size = (200, 300),
+	background = (50, 50, 60),
+	dragable = True,
+	drag_area = [5, 5, 190, 25],
+	drag_area_background = (70, 70, 80)
 )
-elements["info_panel"].rect.bottomleft = app.rect.bottomleft
+elements["panel_drag"].rect.center = app.rect.center
+elements["table_info"] = go.Table(
+	background = (15, 15, 20),
+	border = True,
+	border_color = (200, 200, 210),
+	size = (app.rect.width, 150),
+	rows = (
+		#("Object", elements["text_test"]),
+		("Type", str(type(elements["text_test"]))),
+		("Rect", str(elements["text_test"].rect)),
+		("Test", "Test")
+	)
+)
+elements["table_info"].rect.bottomleft = app.rect.bottomleft
 app.draw_list.add(*[e for _, e in elements.items()])
 # main loop
 def main():
 	while True:
 		# -------------------------------------------------------------------- #
-		#print(app.fps)
 		if app.resized:
-			elements["info_panel"].rect.size = (app.rect.width, 50)
-			elements["info_panel"].rect.bottomleft = app.rect.bottomleft
-		# drawing information to panel
-		for name, elem in elements.items():
-			if elem.hover:
-				panel = elements["info_panel"]
-				info_text = [
-					str(type(elem)),
-					str(elem.rect)
-				]
-				for i in range(len(info_text)):
-					text = go.Text(
-						text = info_text[i],
-						font_size =	13
-					)
-					panel.draw(panel.background, panel.columns[i])
-					panel.draw(text, panel.columns[i])
+			elements["table_info"].resize((app.rect.width, 150))
+			elements["table_info"].rect.bottomleft = app.rect.bottomleft
 		# -------------------------------------------------------------------- #
 		# events
 		if "esc" in app.keys:
 			app.quit()
 		# drawing
-
 		# updating
 		app.update()
-
 if __name__ == '__main__':
     main()
