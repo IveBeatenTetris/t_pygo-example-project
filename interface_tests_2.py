870def89a0d54e8df42f1b4a9bd3665fa0c862d0
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-16 10:07:09 +0100

    testing interface

--- interface_tests_2.py
+++ interface_tests_2.py
@@ -16,11 +16,11 @@ elements["panel"] = go.Panel(
 		background = (50, 50, 60),
 		size = (150, 200),
 		position = (margin, margin),
-		dragable = True
+		dragable = True,
+		drag_area = [5, 5, 140, 25],
+		drag_area_background = (70, 70, 80)
 	)
 elements["table"] = go.Table(
-		rows = 3,
-		cols = 2,
 		size = (150, 50),
 		position = (
 			elements["panel"].rect.right + margin,
@@ -28,6 +28,11 @@ elements["table"] = go.Table(
 		),
 		border = True,
 		border_color = (180, 180, 190),
+		rows = (
+			("Key1", "Value1"),
+			("Key2", "Value2"),
+			("Key3", "Value3")
+		)
 	)
 elements["button"] = go.Button(
 		text =	"New Button",
@@ -42,7 +47,7 @@ elements["button"] = go.Button(
 		font_size =	20,
 	)
 elements["text"] = go.Text(
-		text = "Lorem Ipsum dolor set amet.",
+		text = "Lorem Ipsum dolor sit amet.",
 		position = (
 			elements["button"].rect.right + margin,
 			elements["button"].rect.top
@@ -57,38 +62,12 @@ elements["text_input"] = go.TextField(
 			elements["text"].rect.top
 		)
 	)
-elements["info_panel"] = go.Table(
-	rows = 2,
-	cols = 4,
-	size = (app.rect.width, 50),
-	border = True,
-	background = (50, 50, 60)
-)
-elements["info_panel"].rect.bottomleft = app.rect.bottomleft
 app.draw_list.add(*[e for _, e in elements.items()])
 # main loop
 def main():
 	while True:
 		# -------------------------------------------------------------------- #
-		#print(app.fps)
-		if app.resized:
-			elements["info_panel"].rect.size = (app.rect.width, 50)
-			elements["info_panel"].rect.bottomleft = app.rect.bottomleft
-		# drawing information to panel
-		for name, elem in elements.items():
-			if elem.hover:
-				panel = elements["info_panel"]
-				info_text = [
-					str(type(elem)),
-					str(elem.rect)
-				]
-				for i in range(len(info_text)):
-					text = go.Text(
-						text = info_text[i],
-						font_size =	13
-					)
-					panel.draw(panel.background, panel.columns[i])
-					panel.draw(text, panel.columns[i])
+		print(app.fps)
 		# -------------------------------------------------------------------- #
 		# events
 		if "esc" in app.keys:
