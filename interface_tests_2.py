378b5c2e2ee570c91542ad90e48f0038d64f6355
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-21 21:24:35 +0100

    testing slot-class

--- interface_tests_2.py
+++ interface_tests_2.py
@@ -5,9 +5,9 @@ app	= go.App(
 	title =	"Test Project 0.1",
 	#size =	(580, 650),
 	size = go.getMachineResolution(),
-	background = (35, 35, 45),
+	background_color = (35, 35, 45),
 	resizable =	True,
-	fps = 120
+	fps = 60
 )
 
 margin = 10
@@ -27,12 +27,13 @@ elements["table"] = go.Table(
 			elements["panel"].rect.top
 		),
 		border = True,
-		border_color = (180, 180, 190),
+		#border_color = (180, 180, 190),
 		rows = (
 			("Key1", "Value1"),
 			("Key2", "Value2"),
 			("Key3", "Value3")
-		)
+		),
+		#text_size = 16
 	)
 elements["button"] = go.Button(
 		text =	"New Button",
@@ -41,8 +42,8 @@ elements["button"] = go.Button(
 			elements["table"].rect.top
 		),
 		border = True,
-		background = (40, 45, 35),
-		background_hover = (50, 55, 45),
+		#background = (40, 45, 35),
+		#background_hover = (50, 55, 45),
 		padding = 10,
 		font_size =	20,
 	)
@@ -57,11 +58,25 @@ elements["text"] = go.Text(
 		#background = (0, 10, 20)
 	)
 elements["text_input"] = go.TextField(
-		position = (
-			elements["text"].rect.right + margin,
-			elements["text"].rect.top
-		)
+	position = (
+		elements["text"].rect.right + margin,
+		elements["text"].rect.top
 	)
+)
+elements["slider"] = go.Slider(
+	#size = (200, 50),
+	position = (
+		elements["text_input"].rect.right + margin,
+		elements["text_input"].rect.top
+	)
+)
+elements["slot"] = go.Slot(
+	background_color = (20, 20, 30),
+	position = (
+		elements["slider"].rect.right + margin,
+		elements["slider"].rect.top
+	)
+)
 app.draw_list.add(*[e for _, e in elements.items()])
 # main loop
 def main():
