d8aae7bb94bdc122722087898b2c977fecc52f63
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-29 13:56:39 +0100

    testing resizable bars

--- gui_bar_tests.py
+++ gui_bar_tests.py
@@ -2,14 +2,35 @@
 import t_pygo as go
 import pygame as pg
 # assignments
-app = go.Window()
+app	= go.App(
+	title =	"Bar-Elements Tests",
+	size =	(980, 550),
+	background_color = (35, 35, 45),
+	resizable =	True,
+	fps = 120
+)
+el = {}
+el["bar"] = go.Bar()
+el["menu_bar"] = go.MenuBar2(
+    size = (app.rect.width, 30)
+)
+el["info_bar"] = go.InfoBar2(
+    size = (app.rect.width, 30)
+)
+el["info_bar"].shift(app.rect.bottomleft, "bottomleft")
+el["bar"].shift(app.rect.center, "center")
+app.draw_list.add(*[e for _, e in el.items()])
 # main loop
 def main():
-	while True:
-		# events
-		events = app.events()
-		# updating
-		app.update()
+    while True:
+        # events
+        if app.resized:
+            el["bar"].shift(app.rect.center, "center")
+            el["menu_bar"].resize((app.rect.width, 30))
+            el["info_bar"].resize((app.rect.width, 30))
+            el["info_bar"].shift(app.rect.bottomleft, "bottomleft")
+        # updating
+        app.update()
 
 if __name__ == '__main__':
     main()
