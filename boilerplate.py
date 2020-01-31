a05839b5127ed1d681c8c5633e405ba68153c73b
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-01-31 14:07:56 +0100

    updated boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -3,12 +3,16 @@ import t_pygo as go
 import pygame as pg
 # assignments
 app = go.App()
+elements = {}
+app.draw_list.add(*[e for _, e in el.items()])
 # main loop
 def main():
-	while True:
-		# events
-		# updating
-		app.update()
+    while True:
+        # events
+        if "esc" in app.keys:
+            app.quit()
+        # updating
+        app.update()
 
 if __name__ == '__main__':
     main()
